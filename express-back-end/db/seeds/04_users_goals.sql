46aeae9e9bf0bc7ffee6363fbced9f7f73e7aec3
Harshbir Singh <singh.hveer1@gmail.com>
2022-07-26 03:30:05 -0600

    Create schema and seeds for users_goals

---
+++ express-back-end/db/seeds/04_users_goals.sql
@@ -0,0 +1,28 @@
+INSERT INTO users_goals (user_id, goal_id)
+VALUES (1,3);
+INSERT INTO users_goals (user_id, goal_id)
+VALUES (1,4);
+INSERT INTO users_goals (user_id, goal_id)
+VALUES (1,5);
+
+INSERT INTO users_goals (user_id, goal_id)
+VALUES (2,2);
+
+INSERT INTO users_goals (user_id, goal_id)
+VALUES (3,1);
+INSERT INTO users_goals (user_id, goal_id)
+VALUES (3,2);
+
+INSERT INTO users_goals (user_id, goal_id)
+VALUES (4,2);
+INSERT INTO users_goals (user_id, goal_id)
+VALUES (4,3);
+INSERT INTO users_goals (user_id, goal_id)
+VALUES (4,4);
+
+INSERT INTO users_goals (user_id, goal_id)
+VALUES (4,1);
+INSERT INTO users_goals (user_id, goal_id)
+VALUES (4,2);
+INSERT INTO users_goals (user_id, goal_id)
+VALUES (4,3);
