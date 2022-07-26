f09efa784c9ce6afb7b5a389216114928adb78fe
Harshbir Singh <singh.hveer1@gmail.com>
2022-07-26 05:37:12 -0600

    Delete redundant file

--- express-back-end/db/schema/favourites_goals.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS favourites_goals CASCADE;
-
-CREATE TABLE favourites_goals (
-  id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  goal_id INTEGER REFERENCES goals(id) ON DELETE CASCADE
-);
