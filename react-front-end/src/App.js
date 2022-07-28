c339d37264a03030d58694065a9ee97ecc987b57
Harshbir Singh <singh.hveer1@gmail.com>
2022-07-28 03:19:18 -0600

    Use useEffect to keep user logged-in

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import MyLandingPage from './components/MyLandingPage';
+import Axios from 'axios';
 
 
 
 export const App = () => {
 
+  const [userInfo, setUserInfo] = useState({});
+  useEffect(() => {
+    Axios.get("http://localhost:8080/login").then((response) => {
+      if(response.data.user !== undefined){
+        setUserInfo(response.data.user.rows[0]);
+        console.log(response);
+      } else {
+        console.log(response);
+      }
+      
+      
+    })
+  }, [])
+
   return (
     <div>
-      <MyLandingPage />
+      <MyLandingPage user = {userInfo.first_name}/>
 
     </div>
   )
