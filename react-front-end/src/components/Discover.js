1f038aa3f245a1e16a92b61ef087880e4db2c26a
Edgarsar <edgarsargsyan@hotmail.com>
2022-07-28 18:32:27 +0000

    Create a fetchGoals fx to fetch the goals from backend

--- react-front-end/src/components/Discover.js
+++ react-front-end/src/components/Discover.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
 import DicoverNavbar from './DiscoverNavbar';
 import './discover.css'
 
+
 function Discover() {
+  const [goals, setGoals] = useState([])
+  useEffect(() => {
+    fetchGoals();
+  }, []);
+
+  
+
+  const fetchGoals = async () => {
+    try {
+      const response = await axios.get('/api/goals');
+      console.log(response.data);
+      setGoals(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  console.log(goals);
+
   return (
     <div >
       <DicoverNavbar />
