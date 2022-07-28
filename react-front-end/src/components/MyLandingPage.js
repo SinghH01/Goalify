bc6c49f9be6ae76822eb14862865683b25a3162c
Harshbir Singh <singh.hveer1@gmail.com>
2022-07-28 01:30:39 -0600

    Fetch and diaply logged-in user info from child component

--- react-front-end/src/components/MyLandingPage.js
+++ react-front-end/src/components/MyLandingPage.js
@@ -1,30 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Login from "./Auth/Login";
+import Register from "./Auth/Register";
 
 function MyLandingPage() {
-  return (
-    <div className="landing-page">
-      {/* <div className="overlay"></div> */}
-      <div className="container">
-        <div className="header-area">
-          <div className="logo">Goalify </div>
-          <ul>
-            <li>Discover</li>
-            <li>Login</li>
-            <li>Register</li>
-            {/* <li>contact</li> */}
-          </ul>
+
+
+  //Set the state if user is logged in
+  const [user, setUser] = useState({});
+  function setUserValue(value){
+    setUser(value);
+  }
+
+  useEffect(()=> {
+
+  },[])
+
+  if(user.first_name !== "") {
+    return (
+      <div className="landing-page">
+        {/* <div className="overlay"></div> */}
+        <div className="container">
+          <div className="header-area">
+            <div className="logo">Goalify </div>          
+            
+            <ul>
+              <li>Discover</li>
+              <li>
+              <Login setValue={setUserValue} />
+              </li>
+              <Register />
+  
+              
+  
+              {/* <li>contact</li> */}
+            </ul>
+          </div>
         </div>
-      </div>
-      <div className="introduction-text">
-        <h1>Meet, chat, and share
-          with people who lovewhat you love!</h1>
+        <div className="introduction-text">
+          <h1>    Welcome! {user.first_name}</h1>
+          <h1>Meet, chat, and share
+            with people who lovewhat you love!</h1>
+  
+        </div>  
+      </div>  
+    );
 
-      </div>
+  } else {
 
+    return (
+      <h1>Hi</h1>
+    )
 
-    </div>
+  }
 
-  );
+  
 }
 
 export default MyLandingPage;
