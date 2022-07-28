7922667d8c698e985d93b143f948c1edc10041dc
Harshbir Singh <singh.hveer1@gmail.com>
2022-07-28 03:17:50 -0600

    Create get login route to send session information back

--- express-back-end/routes/login.js
+++ express-back-end/routes/login.js
@@ -6,17 +6,28 @@ const bcrypt = require('bcrypt')
 const saltRounds = 10
 
 module.exports = (db) => {
+
+  router.get("/", (req, res) => {
+    if(req.session.user){
+      res.send ({loggedIn: true, user: req.session.user})
+    } else {
+      res.send({loggedIn: false})
+    }
+  })
+
   router.post("/", (req, res) => {
     values = [req.body.email]
     db.query(`SELECT * FROM users
     WHERE email = $1`, values)
       .then((data)=> {
 
-        console.log(data.rows)
+        //console.log(data.rows)
 
         if(data.rows.length !== undefined) {
           bcrypt.compare(req.body.password, data.rows[0].password, (error, response) => {
             if(response) {
+              req.session.user = data;
+              console.log(req.session.user);
               res.send(data);
             } else {
               res.send({message: "Password didn't match"})
