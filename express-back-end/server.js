53c9b3ce8e342637e45f576064feb2ceb0d664b3
Harshbir Singh <singh.hveer1@gmail.com>
2022-07-28 01:28:47 -0600

    Setup login and register routes

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -6,6 +6,11 @@ const PORT = process.env.PORT || 8080;
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const cors = require("cors")
+
+//Password Encryption
+const bcrypt = require('bcrypt')
+const saltRounds = 10
 
 // PG database client/connection setup
 const { Pool } = require("pg");
@@ -17,19 +22,24 @@ db.connect();
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan("dev"));
+app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.json());
 
 app.use(express.static("public"));
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
+const loginRoutes = require("./routes/login");
+const registerRoutes = require("./routes/register");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
+app.use("/register", registerRoutes(db));
+app.use("/login", loginRoutes(db));
 
 
 app.listen(PORT, () => {
