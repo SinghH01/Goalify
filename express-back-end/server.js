744d86d80f9319b4143f5fef411bda5942ade32a
Harshbir Singh <singh.hveer1@gmail.com>
2022-07-28 03:16:44 -0600

    Setup express cookie session

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -8,6 +8,12 @@ const app = express();
 const morgan = require("morgan");
 const cors = require("cors")
 
+//user-session
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+
+
 //Password Encryption
 const bcrypt = require('bcrypt')
 const saltRounds = 10
@@ -22,10 +28,28 @@ db.connect();
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan("dev"));
-app.use(cors());
+app.use(express.json());
+app.use(cors({
+  origin: ["http://localhost:3000"],
+  methods: ["GET", "POST"],
+  credentials: true,
+}));
+
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.use(session({
+  key: "userId",
+  secret: "userSecret",
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    express: 60 * 60 * 24,
+  },
+}))
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+
 
 app.use(express.static("public"));
 
@@ -34,12 +58,14 @@ app.use(express.static("public"));
 const usersRoutes = require("./routes/users");
 const loginRoutes = require("./routes/login");
 const registerRoutes = require("./routes/register");
+const logoutRoutes = require("./routes/logout");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/register", registerRoutes(db));
 app.use("/login", loginRoutes(db));
+app.use("/logout", logoutRoutes(db));
 
 
 app.listen(PORT, () => {
