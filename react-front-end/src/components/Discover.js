515f41423db8baffefc92708d9c00365d1566e65
Edgarsar <edgarsargsyan@hotmail.com>
2022-07-28 18:39:30 +0000

    Change the Proxy to 8080

--- react-front-end/src/components/Discover.js
+++ react-front-end/src/components/Discover.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
+import axios from 'axios'
 import DicoverNavbar from './DiscoverNavbar';
 import './discover.css'
 
@@ -15,7 +16,7 @@ function Discover() {
   const fetchGoals = async () => {
     try {
       const response = await axios.get('/api/goals');
-      console.log(response.data);
+      console.log(response.data);  
       setGoals(response.data);
     } catch (error) {
       console.log(error);
