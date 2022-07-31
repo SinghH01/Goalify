01296af1d3a07a05aaabcf29c0bfa62736b28185
Harshbir Singh <singh.hveer1@gmail.com>
2022-07-31 11:00:07 -0600

    Generate data to populate in sidebar component

---
+++ react-front-end/src/components/SidebarData.js
@@ -0,0 +1,30 @@
+import React from "react";
+import HomeIcon from '@material-ui/icons/Home';
+import TrackChangesIcon from '@material-ui/icons/TrackChanges';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import PageviewIcon from '@material-ui/icons/Pageview';
+
+
+export const SidebarData = [
+  {
+    title: "Home",
+    icon: <HomeIcon />,
+    link: "/home"
+  },
+  {
+    title: "My Goals",
+    icon: <TrackChangesIcon />,
+    link: "/mygoals"
+  },
+  {
+    title: "Favourites",
+    icon: <FavoriteIcon />,
+    link: "/favourites"
+  },
+  {
+    title: "Discover",
+    icon: <PageviewIcon />,
+    link: "/discover"
+  }
+
+] 
