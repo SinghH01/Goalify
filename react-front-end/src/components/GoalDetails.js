07e83e70cb5465c0b6415ea1f58c308ff2c5ffe3
Edgarsar <edgarsargsyan@hotmail.com>
2022-08-02 16:11:29 +0000

    Create a model for goal detail

--- react-front-end/src/components/GoalDetails.js
+++ react-front-end/src/components/GoalDetails.js
@@ -1,54 +1,68 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import Accordion from 'react-bootstrap/Accordion';
+import Card from 'react-bootstrap/Card';
+
+
 
 function GoalDetails(props) {
   
-  
+
+
   return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Modal heading
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <h4>Centered Modal</h4>
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-          consectetur ac, vestibulum at eros.
-        </p>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
-      </Modal.Footer>
-    </Modal>
+    <>
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+       
+          <Modal.Title id="contained-modal-title-vcenter">
+            {props.goal.title}
+          </Modal.Title>
+        </Modal.Header>
+        <Card>
+        <Card.Img variant="top" src={props.goal.image}  />
+      </Card>
+        <Modal.Body>
+          <h4>Description</h4>
+          <p>
+            {props.goal.description}
+          </p>
+          <h4>MilseStones</h4>
+          <Accordion>
+            <Accordion.Item eventKey="0">
+              <Accordion.Header>{"title"}</Accordion.Header>
+              <Accordion.Body>
+              {/* {props.description} */}
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="1">
+              <Accordion.Header>title</Accordion.Header>
+              <Accordion.Body>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="2">
+              <Accordion.Header>Accordion Item #2</Accordion.Header>
+              <Accordion.Body>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" style={{ width: '66px', height: '42px' }}>Join</Button>
+          {/* <Button onClick={props.onHide}>Close</Button> */}
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 }
 export default GoalDetails
 
 
-// function App() {
-//   const [modalShow, setModalShow] = React.useState(false);
-
-//   return (
-//     <>
-//       <Button variant="primary" onClick={() => setModalShow(true)}>
-//         Launch vertically centered modal
-//       </Button>
-
-//       <GoalDetails
-//         show={modalShow}
-//         onHide={() => setModalShow(false)}
-//       />
-//     </>
-//   );
-// }
-
-// render(<App />);
