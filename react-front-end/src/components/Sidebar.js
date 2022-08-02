431e0e49ec2d845b2b20fdd98c53a3875d55f770
Harshbir Singh <singh.hveer1@gmail.com>
2022-08-01 21:17:59 -0600

    Change the value of onClick function to change state Dashboard

--- react-front-end/src/components/Sidebar.js
+++ react-front-end/src/components/Sidebar.js
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { atom, useRecoilState } from 'recoil';
 import {SidebarData} from "./SidebarData"
 import './sidebar.css'
-export default function Sidebar() {
 
+
+export default function Sidebar({onStateChange}) {
+  
   return (
     <div className='sidebar' >
+
       <ul className='sidebarList'> 
 
       {SidebarData.map((val,key) => {
         return (
             <li 
-            className='row' key = {key} onClick={()=> { window.location.pathname = val.link}}>       
+            className='row' key = {key} onClick={()=> {onStateChange(val.link)}}>       
             <div id="listItems">
             <div id="icon"> {val.icon}</div> 
             <div id = "title"> {val.title}</div>    
