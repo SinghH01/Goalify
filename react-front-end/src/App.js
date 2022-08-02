2f360589eaefaf73987b89ad87c5c0576e459ab8
Harshbir Singh <singh.hveer1@gmail.com>
2022-08-01 21:08:42 -0600

    Implelement conditional rendering of home page and set route for discover page

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,20 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import MyLandingPage from './components/MyLandingPage';
-import Axios from 'axios';
-import Dashboard from './components/Dashboard';
-import Discover from './components/Discover';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { atom, useRecoilState } from 'recoil';ß
+import Axios from 'axios';
+import './App.css';
 
+//Components
+import MyLandingPage from './components/MyLandingPage';
+import Discover from './components/Discover';
+import Dashboard from './components/Dashboard';
+
+
+//Setup global state variable using Recoil JS
+export const userState = atom ({
+  key: 'userState',
+  default: {}
+})
 
 const App = () => {
-  const [userInfo, setUserInfo] = useState({});
+  const [user, setUser] = useRecoilState(userState);
+
   useEffect(() => {
     Axios.get("http://localhost:8080/login").then((response) => {
       if (response.data.user !== undefined) {
-        setUserInfo(response.data.user.rows[0]);
-        
+        setUser(response.data.user.rows[0]);        
         console.log(response);
+     
       } else {
         console.log(response);
       }
@@ -23,7 +33,7 @@ const App = () => {
 
   //Check if user is logged in or not
     let component = undefined;
-      if (userInfo.first_name !== undefined) {
+      if (user.first_name !== undefined) {
         component = <Dashboard />
       } else {
         component = <MyLandingPage />
@@ -33,10 +43,13 @@ const App = () => {
     
     <Router>
     <>
-    {component}
+
+    <Routes>
+        <Route exact path="/discover" element={<Discover />} />
+        <Route exact path="/" element={component} />
+    </Routes>
     </>      
     </Router>
-
     
   )
 
