62fc628f2e5fb9596351a9fe6fb3a0d23c172436
Edgarsar <edgarsargsyan@hotmail.com>
2022-08-03 16:56:26 +0000

    Create a GaolTableRow filse

---
+++ react-front-end/src/components/UserGoals/GoalsTableRow.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import moment from 'moment';
+  
+const GoalsTableRow = (props) => {
+  const { _id, title, description, image,start_date,end_date,online_goal } = props.obj;
+  
+//   const deleteStudent = () => {
+//     axios
+//       .delete(
+// "http://localhost:4000/students/delete-student/" + _id)
+//       .then((res) => {
+//         if (res.status === 200) {
+//           alert("Student successfully deleted");
+//           window.location.reload();
+//         } else Promise.reject();
+//       })
+//       .catch((err) => alert("Something went wrong"));
+//   };
+  
+  return (
+    <tr>
+      <td>{title}</td>
+      <td>{description}</td>
+      <td>
+      < img class="img-fluid img-thumbnail" src={image}/>
+      </td>
+      <td>{moment(start_date).format('MMMM Do, YYYY')}</td>
+      <td>{moment(end_date).format('MMMM Do, YYYY')}</td>
+      
+
+
+
+      <td>
+        <Link className="edit-link" 
+          to={"/edit-student/" + _id}>
+          Edit
+        </Link>
+        <Button onClick={"deleteStudent"} 
+          size="sm" variant="danger">
+          Delete
+        </Button>
+      </td>
+    </tr>
+  );
+};
+  
+export default GoalsTableRow;
