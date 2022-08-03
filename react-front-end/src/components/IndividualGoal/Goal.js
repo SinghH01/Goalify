76f5e595df83b86c0a71791ea241da8d0d93ca7f
Harshbir Singh <singh.hveer1@gmail.com>
2022-08-03 15:46:07 -0600

    Fetch data for goal that is clicked

--- react-front-end/src/components/IndividualGoal/Goal.js
+++ react-front-end/src/components/IndividualGoal/Goal.js
@@ -1,22 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useRecoilState } from 'recoil';
 import { userState } from '../../App';
+import Axios from "axios";
 import Chat from "./Chat";
 import './goal.css'
 
 function Goal(props) {
   const [user, setUser] = useRecoilState(userState);
+  const[goal, setGoal] = useState({message: 'hi'})
+
+  useEffect(() => {
+    fetchGoals();
+  }, []);
+  
+  const fetchGoals = async () => {
+    try {
+      const response = await Axios.post('/api/goals/individualgoal',{ id: props.id});
+      console.log(response)
+      setGoal(response.data[0])
+      console.log(goal.title)
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
 
 
   return (
     <div className="goal-main">
+
       <div className="goal">
-        <h1>Goal ID is: {props.id}</h1>
+        <h1>{goal.title}</h1>
+        <img className="goal-image" src={goal.image} />
       </div>
+
       <div className="chat">
-      <h1>User ID is: {user.id}</h1>
-      <Chat id = {props.id}/>
+        <Chat id = {props.id}/>
       </div>
+
     </div>
 
   );
