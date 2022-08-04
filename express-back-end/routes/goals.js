d482674f879c4a83c8ca1c46419eee9bfc7593d6
Edgarsar <edgarsargsyan@hotmail.com>
2022-08-04 00:14:54 +0000

    Create a post route for goal

--- express-back-end/routes/goals.js
+++ express-back-end/routes/goals.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
@@ -8,7 +8,7 @@ module.exports = (db) => {
         console.log(data.rows)
         const goals = data.rows;
         console.log(typeof goals)
-        res.json( goals );
+        res.json(goals);
       })
       .catch(err => {
         res
@@ -18,13 +18,13 @@ module.exports = (db) => {
   });
 
   router.get("/goal_location/:id", (req, res) => {
-    db.query(`SELECT * FROM goals_locations where goal_id = $1;`,[
+    db.query(`SELECT * FROM goals_locations where goal_id = $1;`, [
       req.params.id
     ])
       .then(data => {
         console.log(data.rows)
         const goalLocation = data.rows;
-        res.json( goalLocation );
+        res.json(goalLocation);
       })
       .catch(err => {
         res
@@ -35,12 +35,28 @@ module.exports = (db) => {
 
 
   router.get("/:id", (req, res) => {
-    db.query(`SELECT * FROM goals where user_id = $1;`,[
+    db.query(`SELECT * FROM goals where user_id = $1;`, [
       req.params.id
     ])
       .then(data => {
         const userGaols = data.rows;
-        res.json( userGaols );
+        res.json(userGaols);
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
+  router.post("/:id", (req, res) => {
+    console.log(req.body)
+    console.log(req.params.id)
+    const values = [req.params.id, req.body.title, req.body.description,req.body.image, req.body.start_date, req.body.end_date]
+    db.query(`INSERT INTO goals (user_id, title, description, image, start_date, end_date)
+      VALUES ($1, $2, $3, $4, $5,$6)`, values)
+      .then(()=> {
+        console.log("goal added to the database");
       })
       .catch(err => {
         res
