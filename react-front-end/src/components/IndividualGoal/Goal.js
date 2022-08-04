85f056ba0f1c50cd3f1b7b3d57ac84a87edea075
Harshbir Singh <singh.hveer1@gmail.com>
2022-08-04 03:39:50 -0600

    Fetch goals location and do conditional rendering based on if the goal has location or not

--- react-front-end/src/components/IndividualGoal/Goal.js
+++ react-front-end/src/components/IndividualGoal/Goal.js
@@ -4,28 +4,35 @@ import { userState } from '../../App';
 import Axios from "axios";
 import Chat from "./Chat";
 import './goal.css'
+import MapContainer from "../Map/MapContainer";
 
 function Goal(props) {
   const [user, setUser] = useRecoilState(userState);
   const[goal, setGoal] = useState({message: 'hi'})
+  const[location, setLocation] = useState(undefined)
+  const [fullAddress, setFullAddress] = useState(undefined)
 
   useEffect(() => {
     fetchGoals();
-  }, []);
-  
+  }, []);  
+
+   
   const fetchGoals = async () => {
     try {
       const response = await Axios.post('/api/goals/individualgoal',{ id: props.id});
-      console.log(response)
+      const goalLocation = await Axios.get(`/api/goals/goal_location/${props.id}`);
+      // console.log(response);
+      // console.log(goalLocation);
       setGoal(response.data[0])
-      console.log(goal.title)
+      setLocation(goalLocation.data[0])
+      setFullAddress(`${goalLocation.data[0].street}, ${goalLocation.data[0].city}, ${goalLocation.data[0].province}`)
+
     } catch (error) {
+
       console.log(error);
+      
     }
   };
-  
-
-
   return (
     <div className="goal-main">
 
@@ -36,6 +43,11 @@ function Goal(props) {
 
       <div className="chat">
         <Chat id = {props.id}/>
+        <div>
+        {/* {location !== undefined && (<h1>This Goal has Physical Location: {fullAddress} </h1>)} */}
+        {location !== undefined && (<MapContainer location = {fullAddress}/>)}
+        {location === undefined && (<h1>This is online goal</h1>)}
+        </div>        
       </div>
 
     </div>
