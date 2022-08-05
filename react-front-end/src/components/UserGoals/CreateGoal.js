99b86e0b07e89309c90e89cd8fbc91181f00e950
Edgarsar <edgarsargsyan@hotmail.com>
2022-08-05 21:41:09 +0000

    Call the handleLoading fx onSubit event

--- react-front-end/src/components/UserGoals/CreateGoal.js
+++ react-front-end/src/components/UserGoals/CreateGoal.js
@@ -8,18 +8,18 @@ const CreateGoal = (props) => {
   const [formValues, setFormValues] =
     useState({ title: '', description: '', image: '', start_date: '', end_date: '' })
   const onSubmit = goalObject => {
+    props.handleLoading();
     Axios.post(
       `http://localhost:8080/api/goals/${props.userId}`,
       goalObject)
       .then(res => {
-       
         window.location.reload(false);
-        
+
       })
       .catch(err => alert('Something went wrong'))
   }
 
-  
+
   return (
     <>
       <h1>Greate New Goal</h1>
