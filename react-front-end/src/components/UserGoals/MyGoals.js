5c0234a312dbb3149eb58ba3a7e5e4370e3dece3
Edgarsar <edgarsargsyan@hotmail.com>
2022-08-07 03:54:06 +0000

    Create a state for milestones and set the state with api data

--- react-front-end/src/components/UserGoals/MyGoals.js
+++ react-front-end/src/components/UserGoals/MyGoals.js
@@ -22,6 +22,7 @@ function MyGoals() {
   const [state, setState] = useState("all")
   const [loading, setLoading] = useState(false)
   const [goal, setGoal] = useState({})
+  const [milestones, setMilestones] = useState([])
 
 
   useEffect(() => {
@@ -31,13 +32,15 @@ function MyGoals() {
   const fetchGoals = async () => {
     try {
       const response = await Axios.get(`/api/goals/${user.id}`);
+      const res = await Axios.get(`/api/milestones`);
       setGoals(response.data);
+      setMilestones(res.data);
     } catch (error) {
       console.log(error);
     }
   };
 
-
+  console.log(milestones)
 
   const DataTable = () => {
     return goals.map((res, i) => {
@@ -50,7 +53,7 @@ function MyGoals() {
   }
 
   const handleEdit = (goal) => {
-    setGoal({...goal})
+    setGoal({ ...goal })
     setState("edit");
   }
 
@@ -66,31 +69,31 @@ function MyGoals() {
   return (<>
     {state === "all" && (
       <>
-          <Button className='createButton' variant="outline-primary" onClick={handleClick}>Create Goal</Button>{' '}
-          <section className="goal-index">
+        <Button className='createButton' variant="outline-primary" onClick={handleClick}>Create Goal</Button>{' '}
+        <section className="goal-index">
           <div className="table-wrapper">
-          <Table striped bordered hover variant="dark">
-            <thead>
-              <tr>
-                <th>Title</th>
-                <th>Description</th>
-                <th>image</th>
-                <th>Start Date</th>
-                <th>End Date</th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody>{DataTable()}</tbody>
-          </Table>
-        </div>
+            <Table striped bordered hover variant="dark">
+              <thead>
+                <tr>
+                  <th>Title</th>
+                  <th>Description</th>
+                  <th>image</th>
+                  <th>Start Date</th>
+                  <th>End Date</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>{DataTable()}</tbody>
+            </Table>
+          </div>
 
-       
+
         </section>
       </>
     )}
 
     {state === "create" && (<CreateGoal userId={user.id} handleLoading={handleLoading} />)}
-    {state === "edit" && (<EditGoal goal={goal} handleLoading={handleLoading}/>)}
+    {state === "edit" && (<EditGoal goal={goal} handleLoading={handleLoading} />)}
 
 
   </>
