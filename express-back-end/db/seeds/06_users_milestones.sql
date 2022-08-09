3b8a6f3093c249b37d09e5c8e1216ed708dbe9f8
Harshbir Singh <singh.hveer1@gmail.com>
2022-08-08 20:29:04 -0600

    Update the seed data for users_milestones

--- express-back-end/db/seeds/06_users_milestones.sql
+++ express-back-end/db/seeds/06_users_milestones.sql
@@ -1,52 +1,52 @@
-INSERT INTO users_milestones (milestone_id, user_id) VALUES (1,3);
-INSERT INTO users_milestones (milestone_id, user_id) VALUES (2,3);
-INSERT INTO users_milestones (milestone_id, user_id) VALUES (3,3);
-INSERT INTO users_milestones (milestone_id, user_id) VALUES (4,3);
+INSERT INTO users_milestones (milestone_id,goal_id, user_id) VALUES (1,1,3);
+INSERT INTO users_milestones (milestone_id,goal_id, user_id) VALUES (2,1,3);
+INSERT INTO users_milestones (milestone_id,goal_id, user_id) VALUES (3,1,3);
+INSERT INTO users_milestones (milestone_id,goal_id, user_id) VALUES (4,1,3);
 
-INSERT INTO users_milestones (milestone_id, user_id) VALUES (1,4);
-INSERT INTO users_milestones (milestone_id, user_id) VALUES (2,4);
-INSERT INTO users_milestones (milestone_id, user_id) VALUES (3,4);
-INSERT INTO users_milestones (milestone_id, user_id) VALUES (4,4);
+INSERT INTO users_milestones (milestone_id,goal_id, user_id) VALUES (1,1,4);
+INSERT INTO users_milestones (milestone_id,goal_id, user_id) VALUES (2,1,4);
+INSERT INTO users_milestones (milestone_id,goal_id, user_id) VALUES (3,1,4);
+INSERT INTO users_milestones (milestone_id,goal_id, user_id) VALUES (4,1,4);
 
-INSERT INTO users_milestones (milestone_id, user_id) VALUES (1,5);
-INSERT INTO users_milestones (milestone_id, user_id) VALUES (2,5);
-INSERT INTO users_milestones (milestone_id, user_id) VALUES (3,5);
-INSERT INTO users_milestones (milestone_id, user_id) VALUES (4,5);
-
-
-
-INSERT INTO users_milestones (milestone_id, user_id) VALUES (9,1);
-INSERT INTO users_milestones (milestone_id, user_id) VALUES (10,1);
-INSERT INTO users_milestones (milestone_id, user_id) VALUES (11,1);
-
-INSERT INTO users_milestones (milestone_id, user_id) VALUES (15,1);
-INSERT INTO users_milestones (milestone_id, user_id) VALUES (16,1);
-INSERT INTO users_milestones (milestone_id, user_id) VALUES (17,1);
-
-INSERT INTO users_milestones (milestone_id, user_id) VALUES (9,3);
-INSERT INTO users_milestones (milestone_id, user_id) VALUES (10,3);
-INSERT INTO users_milestones (milestone_id, user_id) VALUES (11,3);
-
-INSERT INTO users_milestones (milestone_id, user_id) VALUES (9,4);
-INSERT INTO users_milestones (milestone_id, user_id) VALUES (10,4);
-INSERT INTO users_milestones (milestone_id, user_id) VALUES (11,4);
+INSERT INTO users_milestones (milestone_id,goal_id, user_id) VALUES (1,1,5);
+INSERT INTO users_milestones (milestone_id,goal_id, user_id) VALUES (2,1,5);
+INSERT INTO users_milestones (milestone_id,goal_id, user_id) VALUES (3,1,5);
+INSERT INTO users_milestones (milestone_id,goal_id, user_id) VALUES (4,1,5);
 
 
 
-INSERT INTO users_milestones (milestone_id, user_id) VALUES (5,1);
-INSERT INTO users_milestones (milestone_id, user_id) VALUES (6,1);
-INSERT INTO users_milestones (milestone_id, user_id) VALUES (7,1);
-INSERT INTO users_milestones (milestone_id, user_id) VALUES (8,1);
+INSERT INTO users_milestones (milestone_id,goal_id, user_id) VALUES (9,4,1);
+INSERT INTO users_milestones (milestone_id,goal_id, user_id) VALUES (10,4,1);
+INSERT INTO users_milestones (milestone_id,goal_id, user_id) VALUES (11,4,1);
 
-INSERT INTO users_milestones (milestone_id, user_id) VALUES (5,4);
-INSERT INTO users_milestones (milestone_id, user_id) VALUES (6,4);
-INSERT INTO users_milestones (milestone_id, user_id) VALUES (7,4);
-INSERT INTO users_milestones (milestone_id, user_id) VALUES (8,4);
+INSERT INTO users_milestones (milestone_id,goal_id, user_id) VALUES (15,3,1);
+INSERT INTO users_milestones (milestone_id,goal_id, user_id) VALUES (16,3,1);
+INSERT INTO users_milestones (milestone_id,goal_id, user_id) VALUES (17,3,1);
 
-INSERT INTO users_milestones (milestone_id, user_id) VALUES (12,2);
-INSERT INTO users_milestones (milestone_id, user_id) VALUES (13,2);
-INSERT INTO users_milestones (milestone_id, user_id) VALUES (14,2);
+INSERT INTO users_milestones (milestone_id,goal_id, user_id) VALUES (9,4,3);
+INSERT INTO users_milestones (milestone_id,goal_id, user_id) VALUES (10,4,3);
+INSERT INTO users_milestones (milestone_id,goal_id, user_id) VALUES (11,4,3);
 
-INSERT INTO users_milestones (milestone_id, user_id) VALUES (15,2);
-INSERT INTO users_milestones (milestone_id, user_id) VALUES (16,2);
-INSERT INTO users_milestones (milestone_id, user_id) VALUES (17,2);
+INSERT INTO users_milestones (milestone_id,goal_id, user_id) VALUES (9,4,4);
+INSERT INTO users_milestones (milestone_id,goal_id, user_id) VALUES (10,4,4);
+INSERT INTO users_milestones (milestone_id,goal_id, user_id) VALUES (11,4,4);
+
+
+
+INSERT INTO users_milestones (milestone_id,goal_id, user_id) VALUES (5,5,1);
+INSERT INTO users_milestones (milestone_id,goal_id, user_id) VALUES (6,5,1);
+INSERT INTO users_milestones (milestone_id,goal_id, user_id) VALUES (7,5,1);
+INSERT INTO users_milestones (milestone_id,goal_id, user_id) VALUES (8,5,1);
+
+INSERT INTO users_milestones (milestone_id,goal_id, user_id) VALUES (5,5,4);
+INSERT INTO users_milestones (milestone_id,goal_id, user_id) VALUES (6,5,4);
+INSERT INTO users_milestones (milestone_id,goal_id, user_id) VALUES (7,5,4);
+INSERT INTO users_milestones (milestone_id,goal_id, user_id) VALUES (8,5,4);
+
+INSERT INTO users_milestones (milestone_id,goal_id, user_id) VALUES (12,2,2);
+INSERT INTO users_milestones (milestone_id,goal_id, user_id) VALUES (13,2,2);
+INSERT INTO users_milestones (milestone_id,goal_id, user_id) VALUES (14,2,2);
+
+INSERT INTO users_milestones (milestone_id,goal_id, user_id) VALUES (15,3,2);
+INSERT INTO users_milestones (milestone_id,goal_id, user_id) VALUES (16,3,2);
+INSERT INTO users_milestones (milestone_id,goal_id, user_id) VALUES (17,3,2);
