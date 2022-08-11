89c02c8901d066d0c6a17a047f007bdacd4f5308
Harshbir Singh <singh.hveer1@gmail.com>
2022-08-10 22:31:11 -0600

    Add content and images component and import gsap

--- react-front-end/src/components/LandingPage/LandingPage.js
+++ react-front-end/src/components/LandingPage/LandingPage.js
@@ -1,12 +1,22 @@
 import React from "react"
+import Content from "./Content"
 import Header from "./Header"
+import Images from "./Images"
 import "./landingPage.css"
+import {gsap, Power3} from 'gsap'
 
 export default function LandingPage() {
 
+  let tl = new gsap.timeline();
+  let ease = Power3.easeOut();
+
   return (
     <div className="goalify">
-      <Header />
+      <Header timeline = {tl} ease = {ease} />
+      <div className="container">
+        <Content />
+        <Images />
+      </div>
     </div>
   )
 
