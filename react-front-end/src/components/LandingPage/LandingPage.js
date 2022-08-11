0c506dae7114b0d12a1078f6cfbd61bc2672650c
Harshbir Singh <singh.hveer1@gmail.com>
2022-08-10 21:27:01 -0600

    Create landing page component

---
+++ react-front-end/src/components/LandingPage/LandingPage.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Header from "./Header"
+import "./landingPage.css"
+
+export default function LandingPage() {
+
+  return (
+    <div className="goalify">
+      <Header />
+    </div>
+  )
+
+}
