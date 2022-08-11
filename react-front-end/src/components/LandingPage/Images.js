ae9479f644020d65074afe531a9a37fe9d4e6eb5
Harshbir Singh <singh.hveer1@gmail.com>
2022-08-10 22:29:32 -0600

    Create images component

---
+++ react-front-end/src/components/LandingPage/Images.js
@@ -0,0 +1,16 @@
+import React from "react"
+import "./images.css"
+
+export default function Images() {
+
+  return (
+    <div>
+      <div className="images">
+        <div className="box1"></div>
+        <div className="box2"></div>
+        <div className="box3"></div>
+      </div>
+    </div>
+  )
+
+}
