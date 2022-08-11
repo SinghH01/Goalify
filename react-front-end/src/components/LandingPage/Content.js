e7908714851a6fc9a5ab3e21dd3e64044dae33fd
Harshbir Singh <singh.hveer1@gmail.com>
2022-08-10 22:27:49 -0600

    Create content section component

---
+++ react-front-end/src/components/LandingPage/Content.js
@@ -0,0 +1,20 @@
+import React from "react"
+import "./content.css"
+
+export default function Content() {
+
+  return (
+    <div>
+      <div className="content">
+        <h1 className="content-inner-bold" >
+          <div>Hard work beats talent</div>
+          <br />
+          <div>When talent doesnt work hard</div>
+        </h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <button>Discover</button>
+      </div>
+    </div>
+  )
+
+}
