b43d17f85c807a534ec0b6e794b2cbd1419eb306
Edgarsar <edgarsargsyan@hotmail.com>
2022-08-11 18:42:44 +0000

    Remove redundant code

--- express-back-end/routes/active.js
+++ express-back-end/routes/active.js
@@ -5,16 +5,16 @@ const _ = require("lodash");
 
 module.exports = (db) => {
   router.post("/", (req, res) => {
-    let values = [req.body.id]
+    let values = [req.body.id];
 
     db.query(`SELECT goals.*
     FROM goals
     JOIN users_goals ON users_goals.goal_id = goals.id
     JOIN users ON users.id = users_goals.user_id
     WHERE users.id = $1;`, values)
+
       .then(data => {
         let goals = data.rows;
-        console.log("users-goals")
         db.query(`SELECT * FROM goals where user_id = $1;`, [
           req.body.id
         ]).then(data => {
