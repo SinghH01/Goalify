a7be0b0a684916b66b174567682633482102349f
Edgarsar <edgarsargsyan@hotmail.com>
2022-08-12 20:25:45 +0000

    Import button from antd

--- react-front-end/src/components/IndividualGoal/UnfollowGoal.js
+++ react-front-end/src/components/IndividualGoal/UnfollowGoal.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
+import { Button } from 'antd';
 
 
 function UnFollowGoal() {
 
   return (
-    <h4>hello</h4>
-
+    <Button type="primary" danger ghost>
+    Unfollow
+  </Button>
   );
 }
 
