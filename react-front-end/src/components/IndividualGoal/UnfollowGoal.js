e9710d2bee5b12aa8e7271babd178e238cc00cba
Edgarsar <edgarsargsyan@hotmail.com>
2022-08-12 20:33:03 +0000

    Create a unfollowGoal fx which will make axios delete request

--- react-front-end/src/components/IndividualGoal/UnfollowGoal.js
+++ react-front-end/src/components/IndividualGoal/UnfollowGoal.js
@@ -1,11 +1,25 @@
 import React, { useState, useEffect } from "react";
+import Axios from 'axios'
 import { Button } from 'antd';
 
 
 function UnFollowGoal() {
 
+
+  const unfolloweGoal = ({userId, goalId}) => {
+    Axios
+      .delete(
+        `http://localhost:8080/active/delete/`,{ userId: user.id, goalId: goalId })
+      .then((res) => {
+        if (res.status === 204) {
+          alert("success")
+        } else Promise.reject();
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
-    <Button type="primary" danger ghost>
+    <Button type="primary" danger ghost onClick={unfolloweGoal}>
     Unfollow
   </Button>
   );
