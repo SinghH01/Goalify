65803015aba7813620df75adec7a585cec853398
Edgarsar <edgarsargsyan@hotmail.com>
2022-08-13 02:57:52 +0000

    Open Notifcation  for success goal unfollow

--- react-front-end/src/components/IndividualGoal/UnfollowGoal.js
+++ react-front-end/src/components/IndividualGoal/UnfollowGoal.js
@@ -2,9 +2,10 @@ import React, { useContext } from "react";
 import DashboardContext from "../DashBoardContext";
 import Axios from 'axios'
 import { Button } from 'antd';
+import { openNotificationWithIcon } from "../Notification";
 
 
-function UnFollowGoal({ userId, goalId }) {
+function UnFollowGoal({ userId, goalId, goalTitle }) {
 
   const providerValue = useContext(DashboardContext)
   const { setState } = providerValue
@@ -16,6 +17,7 @@ function UnFollowGoal({ userId, goalId }) {
         `http://localhost:8080/active/delete`, { userId, goalId })
       .then((res) => {
         if (res.status === 204) {
+          openNotificationWithIcon("error", <> You Unfollow  the Goal <strong>{goalTitle}</strong> </>);
           setState("activegoals")
         } else Promise.reject();
       })
