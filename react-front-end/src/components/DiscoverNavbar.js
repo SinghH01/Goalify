51d342e28e5d652fa52c7a35b36b96043fec720b
Harshbir Singh <singh.hveer1@gmail.com>
2022-08-13 10:15:51 -0600

    Remove toggle option from navbar

--- react-front-end/src/components/DiscoverNavbar.js
+++ react-front-end/src/components/DiscoverNavbar.js
@@ -31,7 +31,6 @@ function DicoverNavbar({ searchQuery, setSearchQuery }) {
       }}><GiStairsGoal size='2rem' /> Goalify
 
       </Navbar.Brand>
-      <Navbar.Toggle aria-controls="navbarScroll" />
       <Navbar.Collapse id="navbarScroll">
         <Nav
           className="me-auto my-2 my-lg-0"
