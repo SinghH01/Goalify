5ca129cc5ae319acc3593d3e333d71cc04518604
Edgarsar <edgarsargsyan@hotmail.com>
2022-08-13 04:16:09 +0000

    Define initial values

--- react-front-end/src/components/UserGoals/EditGoals.js
+++ react-front-end/src/components/UserGoals/EditGoals.js
@@ -1,17 +1,33 @@
-import React  from "react";
+import React, { useState, useEffect } from "react";
 import Axios from "axios";
 import GoalForm from "./GoalForm";
-import { openNotificationWithIcon } from "../Notification";
+import { notification } from 'antd';
+
 
 
 
 const EditGoal = (props) => {
 
+  const openNotificationWithIcon = (type) => {
+    notification[type]({
+      message: 'Goalify',
+      description: (
+        <>
+          Thank you {props.userName}.<br />
+          Your Gaol has been successfully Edited!!!
+        </>
+      )
+    });
+  };
+
+
   const formValues = {
     title: props.goal.title,
     description: props.goal.description,
-    image: props.goal.image
-  };
+    image: "",
+    start_date: "",
+    end_date: ""
+  }
 
   const onSubmit = (GoalObject) => {
     props.setState("loading");
@@ -23,13 +39,7 @@ const EditGoal = (props) => {
       .then((res) => {
         if (res.status === 204) {
           props.setState("all");
-          openNotificationWithIcon(
-            "warning",
-            <>
-          Thank you <strong>{props.userName}</strong>.<br />
-          Your Gaol has been successfully Edited!!!
-        </>  
-            )
+          openNotificationWithIcon("warning")
         } else Promise.reject();
       })
       .catch((err) => alert("Something went wrong"));
