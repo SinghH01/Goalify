6d00c3c6bcc086227a7ddae892dcc02c977b6be6
Edgarsar <edgarsargsyan@hotmail.com>
2022-08-15 03:27:14 +0000

    Change the form style

--- react-front-end/src/components/UserGoals/MilestoneForm.js
+++ react-front-end/src/components/UserGoals/MilestoneForm.js
@@ -1,59 +1,79 @@
 import React from "react";
 import * as Yup from "yup";
 import { Formik, Form, Field, ErrorMessage } from "formik";
-import { FormGroup, FormControl, Button } from "react-bootstrap";
+import { FormGroup, Button } from "react-bootstrap";
+import { Button as AntdButton, Input, Upload, DatePicker, Space } from 'antd';
 
+const { TextArea } = Input;
 
 const MilestoneForm = (props) => {
 
   const validationSchema = Yup.object().shape({
     title: Yup.string().required(" Title Can't be Balnk"),
     description: Yup.string().required("Description Can't be Balnk"),
-    // image: Yup.string().required("Required"),
     end_date: Yup.date().required("End Date Required")
   });
 
   return (
     <div className="form-wrapper">
-      
+
       <Formik {...props} validationSchema={validationSchema}>
-        <Form>
-          <FormGroup>
-              <label htmlFor="title">Title</label>
-              <Field name="title" className="form-control" type="text" />
-           
-            <ErrorMessage
-              name="title"
-              className="d-block invalid-feedback"
-              component="span"
-            />
-          </FormGroup>
-          <FormGroup>
-            <label htmlFor="description">Description</label>
-            <Field name="description" className="form-control" as="textarea" rows={3} cols={10} />
-            <ErrorMessage
-              name="description"
-              className="d-block invalid-feedback"
-              component="span"
-            />
-          </FormGroup>
-          
-          <FormGroup>
-          <label>End Date</label>
-            <Field name="end_date" type="date"
-              className="form-control" />
-            <ErrorMessage
-              name="end_date"
-              className="d-block invalid-feedback"
-              component="span"
-            />
-          </FormGroup>
-          <div className="form-submit">
+        {({ handleChange, values, setFieldValue }) => (
+          <Form>
+            <FormGroup>
+              <Input
+                showCount maxLength={20}
+                name="title"
+                type="text"
+                placeholder="Title"
+                value={values.title}
+                onChange={handleChange} />
+              <ErrorMessage
+                name="title"
+                className="d-block invalid-feedback"
+                component="span"
+              />
+            </FormGroup>
+            <br />
+            <FormGroup>
+              <TextArea
+                showCount maxLength={140}
+                name="description"
+                placeholder="Description"
+                value={values.description}
+                onChange={handleChange} />
+              <ErrorMessage
+                name="description"
+                className="d-block invalid-feedback"
+                component="span"
+              />
+            </FormGroup>
+            <br />
+            <FormGroup>
+              <Space direction="vertical" size={12}>
+                <DatePicker
+                  showTime
+                  name="end_date"
+                  placeholder="End Date"
+                  selected={(values.end_date && new Date(values.start_date)) || null}
+                  onChange={(value, dateString) =>
+                    setFieldValue("end_date", dateString)}
+                />
+              </Space>
+              <ErrorMessage
+                name="end_date"
+                className="d-block invalid-feedback"
+                component="span"
+              />
+            </FormGroup>
+            <br />
+            <div className="form-submit">
               <Button variant="outline-success" block="block" type="submit">
                 {props.children}
               </Button>{' '}
             </div>
-        </Form>
+          </Form>
+        )}
       </Formik>
     </div>
   );
