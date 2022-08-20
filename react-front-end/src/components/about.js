a0f4b8e19d291b4d45a75279fb880d33d1f64542
Harshbir Singh <singh.hveer1@gmail.com>
2022-08-20 12:58:17 -0600

    Remove about and tech stack sections and add names to profile section

--- react-front-end/src/components/about.js
+++ react-front-end/src/components/about.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import './about.css'
+import React from "react";
+import "./about.css";
 import { AiFillGithub } from "react-icons/ai";
 import { AiFillLinkedin } from "react-icons/ai";
 import { BsFillTelephoneForwardFill } from "react-icons/bs";
@@ -7,95 +7,95 @@ import { AiOutlineMail } from "react-icons/ai";
 
 const About = () => {
   return (
-    <div className='body'>
-      <div className='header'>
-        <h1>about us</h1>
-        <p>
-          A web ...
-        </p>
-
-      </div>
-      <div className='header'>
-        <h1>Tech Stack</h1>
-        <p>
-          <ul>
-            <li> Frontend : React, CSS, Bootstrap</li>
-            <li> Backend : Express,  Nodejs ,Socket.IO</li>
-            <li> Database : PostgreSQL</li>
-          </ul>
-        </p>
-      </div>
-      <div className='header'>
-        <h1>Features</h1>
-        <p>
-          <ul>
-            <li> Login and explore goals listings</li>
-            <li> Search for goals </li>
-            <li> Logged-in users can fav goals and view them later</li>
-            <li> user can create, delete and edit goal listings</li>
-          </ul>
-        </p>
-      </div>
-      <div className='about-us'>
-        <div className='profile'>
-          <img src={require('../assets/avatar2.jpeg')} alt='avatar' className='avatar' />
+    <div className="about-body">
+      <div className="about-us">
+        <div className="profile">
+          <img
+            src={require("../assets/avatar2.jpeg")}
+            alt="avatar"
+            className="avatar"
+          />
+          <h3>Harshbir Singh</h3>
           <ul>
             <li>
-              <a href="https://www.linkedin.com/in/huwaida-khalid-271190242/" className="icon fa-twitter alt">
+              <a
+                href="https://www.linkedin.com/"
+                className="icon fa-twitter alt"
+              >
                 <AiFillLinkedin /> LinkedIn profile
               </a>
             </li>
             <li>
               <a
-                href="https://github.com/Huwaida1990"
+                href="https://github.com/SinghH01"
                 className="icon fa-github alt"
-              ><AiFillGithub /> GitHub profile</a>
+              >
+                <AiFillGithub /> GitHub profile
+              </a>
             </li>
 
             <li>
-              <a href="https://wa.me/+14039289813">
-                <BsFillTelephoneForwardFill /> +14039289813
+              <a href="">
+                <BsFillTelephoneForwardFill /> 587-222-1202
               </a>
             </li>
             <li>
-              <a href="mailto:huwaida_khaild@yahoo.com" className="">
-                <AiOutlineMail /> huwaida_khaild@yahoo.com
+              <a href="mailto:singh.hveer1@gmail.com" className="">
+                <AiOutlineMail /> singh.hveer1@gmail.com
               </a>
             </li>
           </ul>
         </div>
-        <div className='profile'>
-          <img src={require('../assets/avatar2.jpeg')} alt='avatar' className='avatar' />
+        <div className="profile">
+          <img
+            src={require("../assets/avatar2.jpeg")}
+            alt="avatar"
+            className="avatar"
+          />
+          <h3>Edgar Sargsyan</h3>
           <ul>
             <li>
-              <a href="https://www.linkedin.com/in/huwaida-khalid-271190242/" className="icon fa-twitter alt">
+              <a
+                href="https://www.linkedin.com/"
+                className="icon fa-twitter alt"
+              >
                 <AiFillLinkedin /> LinkedIn profile
               </a>
             </li>
             <li>
               <a
-                href="https://github.com/Huwaida1990"
+                href="https://github.com/Edgarsar"
                 className="icon fa-github alt"
-              ><AiFillGithub /> GitHub profile</a>
+              >
+                <AiFillGithub /> GitHub profile
+              </a>
             </li>
 
             <li>
               <a href="https://wa.me/+14039289813">
-                <BsFillTelephoneForwardFill /> +14039289813
+                <BsFillTelephoneForwardFill /> 306-216-9949
               </a>
             </li>
             <li>
-              <a href="mailto:huwaida_khaild@yahoo.com" className="">
-                <AiOutlineMail /> huwaida_khaild@yahoo.com
+              <a href="mailto:edgarsargsyan@hotmail.com" className="">
+                <AiOutlineMail /> edgarsargsyan@hotmail.com
               </a>
             </li>
           </ul>
         </div>
-        <div className='profile'>
-          <img src={require('../assets/avatar1.jpeg')} alt='avatar' className='avatar' />
+        <div className="profile">
+          <img
+            src={require("../assets/avatar1.jpeg")}
+            alt="avatar"
+            className="avatar"
+          />
+          <h3>Huwaida Khalid</h3>
           <ul>
             <li>
-              <a href="https://www.linkedin.com/in/huwaida-khalid-271190242/" className="icon fa-twitter alt">
+              <a
+                href="https://www.linkedin.com/in/huwaida-khalid-271190242/"
+                className="icon fa-twitter alt"
+              >
                 <AiFillLinkedin /> LinkedIn profile
               </a>
             </li>
@@ -103,7 +103,9 @@ const About = () => {
               <a
                 href="https://github.com/Huwaida1990"
                 className="icon fa-github alt"
-              ><AiFillGithub /> GitHub profile</a>
+              >
+                <AiFillGithub /> GitHub profile
+              </a>
             </li>
 
             <li>
@@ -120,7 +122,7 @@ const About = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
