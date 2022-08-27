83a9f66063873c53647e7198bf2f0d1c2945865f
Harshbir Singh <singh.hveer1@gmail.com>
2022-08-27 17:10:45 -0600

    Remove react icon import

--- react-front-end/src/components/DiscoverNavbar.js
+++ react-front-end/src/components/DiscoverNavbar.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { GiStairsGoal } from "react-icons/gi";
 import Button from 'react-bootstrap/Button';
 import axios from 'axios'
 import './navbar.css'
