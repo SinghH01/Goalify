e2d64b7a8bd23a570a07f599103a3620bec9bd97
Harshbir Singh <singh.hveer1@gmail.com>
2022-08-30 22:44:13 -0600

    Remove unused imports

--- react-front-end/src/components/Map/LocationMarker.js
+++ react-front-end/src/components/Map/LocationMarker.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Icon } from "@iconify/react";
 import locationIcon from "@iconify/icons-mdi/location-favorite";
 import "../../styles/map.css";
+
 const LocationMarker = ({ lat, lng }) => {
   return (
     <div className="location-marker">
