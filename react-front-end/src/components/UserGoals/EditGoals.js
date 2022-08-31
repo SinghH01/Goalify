77bf1726ecde7131037d186311c89fdc0d69e31e
Harshbir Singh <singh.hveer1@gmail.com>
2022-08-30 22:46:31 -0600

    Remove redundant imports

--- react-front-end/src/components/UserGoals/EditGoals.js
+++ react-front-end/src/components/UserGoals/EditGoals.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Axios from "axios";
 import GoalForm from "./GoalForm";
 import { openNotificationWithIcon } from "../Notification";
